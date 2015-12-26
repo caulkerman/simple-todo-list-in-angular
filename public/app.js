9f9398c140ebf07efe6a50c4db2c8d5bb13b5eab
caulkerman <ih8blackwidowspiders@yahoo.com>
2015-12-25 19:03:57 -0700

    made a little todo list

---
+++ public/app.js
@@ -0,0 +1,32 @@
+//APP//
+angular.module("app", []);
+
+//CONTROLLER//
+(function() {
+	var $inject = ["$scope"];
+	function controllerCB($scope) {
+
+		/////Start your coding here//////
+		// $scope.test = "this is controller test";
+		
+		$scope.itemsArray = [];
+		
+		$scope.addItems = function(items) {
+			// console.log(items);
+			$scope.itemsArray.push(items);
+			// console.log($scope.itemsArray);
+			$scope.items = "";
+		}
+
+		$scope.deleteItem = function(index) {
+			$scope.itemsArray.splice(index, 1);
+			// console.log(index);
+			// console.log($scope.itemsArray);
+		}
+
+		
+		//////End your coding here///////
+	}
+	controllerCB.$inject = $inject;
+	angular.module("app").controller("controller", controllerCB)
+})();
